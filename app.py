69bef673d2c9f481bd44ad3c211cc54cc5e8159f
FelipeAPM <35493876+casfe03@users.noreply.github.com>
2023-10-01 18:24:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,30 +6,30 @@ app = Flask(__name__)
 # Contador para gerar a sequência de protocolo
 contador_protocolo = 1
 
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     global contador_protocolo  # referência ao contador global
     
-    if 'file' not in request.files:
-        return {"error": "Nenhum arquivo enviado"}, 400
+    data = request.get_data()  # Obtem os dados binários brutos do corpo da requisição
     
-    file = request.files['file']
-    if file.filename == '':
-        return {"error": "Arquivo sem nome"}, 400
-
-    if not file or not file.filename.endswith('.pdf'):
-        return {"error": "Formato de arquivo inválido. Apenas PDFs são permitidos"}, 400
+    if not data:
+        return {"error": "Nenhum dado enviado"}, 400
 
     # Gerar o número de protocolo
     protocolo = f"TDFT2023{contador_protocolo:04d}"  # :04d garante que o número tenha sempre 4 dígitos, preenchendo com zeros à esquerda se necessário
     
     # Incrementar o contador de protocolo
     contador_protocolo += 1
-
+    
+    # Você pode modificar isso para salvar com o nome que preferir, aqui estou usando o número de protocolo como nome do arquivo
+    filename = f"{protocolo}.pdf"
+    
+    with open(filename, 'wb') as f:
+        f.write(data)  # Escreve os dados binários no arquivo
+    
     # Data atual
     data_atual = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    
+
     return {
         "mensagem": f"Recebemos seu pedido com sucesso no SEI.",
         "data_recebimento": data_atual,
@@ -38,4 +38,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=5000)
